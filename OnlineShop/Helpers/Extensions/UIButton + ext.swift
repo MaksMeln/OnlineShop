830aecaa8be57d8d41c1a4f3e272e81e69b551b3
MaksMeln <maksim.melnichuk_13@mail.ru>
2023-03-13 21:41:10 +0300

    change UIButton + ext

--- OnlineShop/Helpers/Extensions/UIButton + ext.swift
+++ OnlineShop/Helpers/Extensions/UIButton + ext.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 public enum OnlineShopButtonType {
-    case uploadItem
     case registration
-    
+    case uploadItem
+    case profileButton
 }
 
 class OnlineShopButton : UIButton {
@@ -20,6 +20,8 @@ class OnlineShopButton : UIButton {
     
     private let label = UILabel()
     private let iconView = UIImageView ()
+    private let vectorView = UIImageView ()
+    private let balance = UILabel()
     
     
     init(with type: OnlineShopButtonType) {
@@ -39,9 +41,11 @@ class OnlineShopButton : UIButton {
         configureAppearance()
     }
     
-    func setInfo(_ title: String?, _ image: UIImage?) {
+    func setInfo(_ title: String?, _ iconViewImage: UIImage?, _ vectorViewImage: UIImage? , _ balanceLabel: String?) {
         label.text = title
-        iconView.image = image
+        iconView.image = iconViewImage
+        vectorView.image = vectorViewImage
+        balance.text = balanceLabel
     }
 }
 
@@ -50,6 +54,8 @@ private extension OnlineShopButton {
     func setupViews() {
         setupView(label)
         setupView(iconView)
+        setupView(vectorView)
+        setupView(balance)
     }
     
     //MARK: - CONSTRAINTVIEWS
@@ -66,24 +72,54 @@ private extension OnlineShopButton {
                 label.centerYAnchor.constraint(equalTo: centerYAnchor),
                 label.leadingAnchor.constraint(equalTo: iconView.trailingAnchor, constant: 40),
             ])
+            
+        case .profileButton:
+            setDimensions(height: 40, width: 298)
+            iconView.setDimensions(height: 40, width: 40)
+            
+            NSLayoutConstraint.activate([
+                
+                iconView.leadingAnchor.constraint(equalTo: leadingAnchor),
+                iconView.centerYAnchor.constraint(equalTo: centerYAnchor),
+                
+                label.centerYAnchor.constraint(equalTo: centerYAnchor),
+                label.leadingAnchor.constraint(equalTo: iconView.trailingAnchor, constant: 9),
+                
+                vectorView.trailingAnchor.constraint(equalTo: trailingAnchor),
+                vectorView.centerYAnchor.constraint(equalTo: centerYAnchor),
+                
+                balance.trailingAnchor.constraint(equalTo: trailingAnchor),
+                balance.centerYAnchor.constraint(equalTo: centerYAnchor),
+            ])
+            
+            
         case .registration :
-           break
-           
+            break
         }
     }
     //MARK: - CONFIGUREAPPEARANCE
     func configureAppearance() {
         switch type {
+            
         case .uploadItem:
             backgroundColor = Resources.Colors.backgroundButton
             layer.cornerRadius = 15
+            
+            label.text = Resources.String.ProfileController.buttonText
             label.textColor = Resources.Colors.defaultWhite
             label.font = Resources.Fonts.MontserratBold(with: 14)
-            label.textAlignment = .center
+            iconView.image = Resources.Images.ProfileController.uploadItem
+            
+        case .profileButton:
+            backgroundColor = .none
+            
+            label.textColor = Resources.Colors.defaultBlack
+            label.font = Resources.Fonts.MontserratMedium(with: 14)
+            balance.textColor = Resources.Colors.defaultBlack
+            balance.font = Resources.Fonts.MontserratMedium(with: 14)
             
         case .registration:
-           break
-    
+            break
         }
         makeSystem(self)
     }

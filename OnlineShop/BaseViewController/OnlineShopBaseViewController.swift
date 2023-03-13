86f8254856a926909c3c55fcfcc62ac477c4c1e2
MaksMeln <maksim.melnichuk_13@mail.ru>
2023-03-13 14:48:40 +0300

    Creation BaseViewController

---
+++ OnlineShop/BaseViewController/OnlineShopBaseViewController.swift
@@ -0,0 +1,40 @@
+//
+//  OnlineShopBaseViewController.swift
+//  OnlineShop
+//
+//  Created by Максим Мельничук on 13.03.23.
+//
+
+import UIKit
+
+class OnlineShopBaseViewController: UIViewController {
+    
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            setupViews()
+            constraintViews()
+            configureAppearence()
+        }
+    }
+
+    @objc extension OnlineShopBaseViewController {
+        
+        func setupViews() {}
+        func constraintViews() {}
+        func configureAppearence() {
+            view.backgroundColor = Resources.Colors.backgroundColor
+        }
+        
+        func leftBarButtonTapped() {
+            print("left button tapped")
+        }
+        
+        func rightBarButtonTapped() {
+            print("")
+        }
+        
+    }
+
+
+

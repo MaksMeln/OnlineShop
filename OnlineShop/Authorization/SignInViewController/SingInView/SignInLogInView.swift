78dabbeecbeaecdc2fb6e85df81797cb2eab7f3f
MaksMeln <maksim.melnichuk_13@mail.ru>
2023-03-15 11:01:27 +0300

    Created classView SignInLogInView

---
+++ OnlineShop/Authorization/SignInViewController/SingInView/SignInLogInView.swift
@@ -0,0 +1,53 @@
+//
+//  SignInLogInView.swift
+//  OnlineShop
+//
+//  Created by Максим Мельничук on 15.03.23.
+//
+
+import UIKit
+
+class SignInLogInView : OnlineShopBaseView {
+    
+    
+    private let subtitleLabelLogIn = UILabel(text: Resources.String.Authorization.subtitleLogIn,
+                                             font: Resources.Fonts.MontserratMedium(with: 10),
+                                             textColor: Resources.Colors.defaultGray)
+    
+    private let logInButton : UIButton = {
+        let button = UIButton()
+        button.setTitle(Resources.String.Authorization.logIn, for: .normal)
+        button.setTitleColor(Resources.Colors.defaultBlue, for: .normal)
+        button.titleLabel?.font = Resources.Fonts.MontserratMedium(with: 10)
+        return button
+    }()
+}
+
+
+extension SignInLogInView {
+    override func setupViews() {
+        super.setupViews()
+    }
+    
+    override func constraintViews() {
+        super.constraintViews()
+        let stackViewLoginInfo = UIStackView(arrangedSubviews: [subtitleLabelLogIn, logInButton])
+        stackViewLoginInfo.axis = .horizontal
+        stackViewLoginInfo.spacing = 9
+        stackViewLoginInfo.heightAnchor.constraint(equalToConstant: 10).isActive = true
+        self.setupView(stackViewLoginInfo)
+        
+        
+        NSLayoutConstraint.activate([
+            stackViewLoginInfo.topAnchor.constraint(equalTo: topAnchor),
+            stackViewLoginInfo.bottomAnchor.constraint(equalTo: bottomAnchor),
+            stackViewLoginInfo.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackViewLoginInfo.leadingAnchor.constraint(equalTo: leadingAnchor),
+            
+        ])
+    }
+    
+    override func configureAppearance() {
+        super.configureAppearance()
+    }
+}

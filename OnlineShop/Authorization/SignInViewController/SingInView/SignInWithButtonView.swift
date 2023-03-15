1b8a0281852323d55adf2b4e79c65444e67849c7
MaksMeln <maksim.melnichuk_13@mail.ru>
2023-03-15 11:02:20 +0300

    Created  classView SignInWithButtonView

---
+++ OnlineShop/Authorization/SignInViewController/SingInView/SignInWithButtonView.swift
@@ -0,0 +1,60 @@
+//
+//  SignInWithButtonView.swift
+//  OnlineShop
+//
+//  Created by Максим Мельничук on 15.03.23.
+//
+
+import UIKit
+
+class SignInWithButtonView : OnlineShopBaseView {
+    
+    
+    private let signInWithGoogleButton : OnlineShopButton = {
+          let button = OnlineShopButton(with: .signInButton)
+          button.setInfo(Resources.String.Authorization.google,
+                         Resources.Images.Autorization.googleIcon,
+                         nil,
+                         nil)
+          return button
+      }()
+  
+      private let signInWithAppleButton : OnlineShopButton = {
+          let button = OnlineShopButton(with: .signInButton)
+          button.setInfo(Resources.String.Authorization.apple,
+                         Resources.Images.Autorization.appleIcon,
+                         nil,
+                         nil)
+          return button
+      }()
+    
+}
+
+
+extension SignInWithButtonView {
+    override func setupViews() {
+        super.setupViews()
+    }
+    
+    override func constraintViews() {
+        super.constraintViews()
+        
+        let stackViewSignInButton = UIStackView(arrangedSubviews: [signInWithGoogleButton , signInWithAppleButton])
+        stackViewSignInButton.axis = .vertical
+        stackViewSignInButton.spacing = 38
+        self.setupView(stackViewSignInButton)
+        
+        NSLayoutConstraint.activate([
+            stackViewSignInButton.topAnchor.constraint(equalTo: topAnchor),
+            stackViewSignInButton.bottomAnchor.constraint(equalTo: bottomAnchor),
+            stackViewSignInButton.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackViewSignInButton.leadingAnchor.constraint(equalTo: leadingAnchor),
+            
+        ])
+    }
+    
+    override func configureAppearance() {
+        super.configureAppearance()
+    }
+}
+
